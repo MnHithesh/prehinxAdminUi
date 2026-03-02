105dcedcfb78c00bfcfe2e785d7f914fa417893d
Hithesh Nandeesh <m.n.hithesh26@gmail.com>
2026-03-02 22:19:22 +0530

    Initial setup: angualr 20 admin panel with tailwind css just a login page created

---
+++ src/app/pages/auth/login/login.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class LoginComponent {
+   private fb = inject(FormBuilder);
+
+   hidePassword = true;
+  isSubmitting = false;
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    rememberMe: [false]
+  });
+
+   onSubmit() {
+    if (this.loginForm.invalid) return;
+    this.isSubmitting = true;
+  }
+}
